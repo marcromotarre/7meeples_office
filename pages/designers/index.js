48bdd48b31b579332ed54040db682d3601627ad8
Marc Romo <marcromotarre@gmail.com>
2021-04-13 10:41:22 +0200

    ordered designers

--- pages/designers/index.js
+++ pages/designers/index.js
@@ -3,6 +3,7 @@
 import { jsx } from "theme-ui";
 import React, { useState, useEffect } from "react";
 import { get_designers } from "../../src/api/designers";
+import { get_boardgames } from "../../src/api/boardgames";
 
 import Table from "../../src/components/common/table";
 
@@ -17,11 +18,24 @@ export default function Designers() {
   }, []);
 
   let getDesigners = async () => {
+    const boardgames = await get_boardgames();
     const designers = await get_designers();
     if (designers) {
       setDesigners(
         designers
-          .sort((a, b) => (a.id > b.id ? 1 : -1))
+          .map((designer) => {
+            const points = boardgames
+              .filter((boardgame) => boardgame.designers.includes(designer.id))
+              .reduce((acc, { numVotes }) => acc + numVotes, 0);
+            return {
+              ...designer,
+              points,
+              description: designer?.description
+                ?.substring(0, 70)
+                .concat("..."),
+            };
+          })
+          .sort((a, b) => (a.points > b.points ? -1 : 1))
           .map((designer, index) => {
             return { ...designer, index: index + 1 };
           })
@@ -31,9 +45,10 @@ export default function Designers() {
 
   const columns = [
     { name: " ", field: "index", width: "min-content" },
+    { name: "points", field: "points", width: "min-content" },
     { name: "ID", field: "id", width: "min-content" },
-    { name: "Nombre", field: "name", width: "auto" },
-    { name: "Imagen", field: "image" },
+    { name: "Nombre", field: "name", width: "min-content" },
+    { name: "Imagen", field: "image", width: "min-content" },
     { name: "Description", field: "description" },
   ];
 
